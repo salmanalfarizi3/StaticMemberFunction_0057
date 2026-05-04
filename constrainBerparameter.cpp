4da9a14c041799ed56260e2ea384124c151f6131
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-05-04 18:43:18 +0700

    Menampilkan Output

--- constrainBerparameter.cpp
+++ constrainBerparameter.cpp
@@ -15,3 +15,9 @@ mahasiswa::mahasiswa(int nim, string nama){
     cout << "Nama : "<<nama<<endl;
 
 }
+int main(){
+    //memanggil Construktor dg parameter
+    mahasiswa mhs(12345, "Pascal");
+    return 0;
+
+}
