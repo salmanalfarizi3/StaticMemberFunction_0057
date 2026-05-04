1e3e9f089e766e7f1b0af9a1f5257cd4a9e07111
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-05-04 18:31:18 +0700

    Membuat Prosedur PrintALL

--- staticmember.cpp
+++ staticmember.cpp
@@ -24,3 +24,9 @@ int Mahasiswa::nim = 0;
 void Mahasiswa::setID() {
     id = ++nim;
 }
+
+void Mahasiswa::printAll() {
+    cout << "ID    = " << id << endl;
+    cout << "Nama  = " << nama << endl;
+    cout << endl;
+}
