774de254f791fdb20150f026974a72eb459ffd57
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-05-04 18:48:40 +0700

    Prosedur cetakdata

--- dectructor.cpp
+++ dectructor.cpp
@@ -27,3 +27,12 @@ angka::~angka(){ // Destructor
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
 
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+v
