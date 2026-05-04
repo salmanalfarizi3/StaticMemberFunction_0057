2af33082bb0e634e36dccbd89277136c3bd29109
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-05-04 18:39:33 +0700

    Menampilkan

--- construktorloding.cpp
+++ construktorloding.cpp
@@ -33,3 +33,14 @@ void mahasiswa::cetak(){
     cout << "Nama:"<< nama<<endl;
 
 }
+int main(){
+    mahasiswa mhs1(102030);// hanya NIM
+    mahasiswa mhs2("Andi");// hanya nama
+    mahasiswa mhs3(010203,"Santi");// nim dan nama
+
+    //tampilkan nilai
+    cout << "Mahasiswa 1:";mhs1.cetak();
+    cout << "Mahasiswa 2:";mhs2.cetak();
+    cout << "Mahasiswa 3:";mhs3.cetak();
+    return 0;
+}
