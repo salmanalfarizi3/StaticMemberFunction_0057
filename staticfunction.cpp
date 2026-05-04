951030922b6a63b86c64a688a1df884f20bddca9
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-05-04 18:55:42 +0700

    Class Mahasiswa Dengan akses Public

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -6,3 +6,24 @@ class Mahasiswa {
 
 private:
     static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    //pembuatan static function
+    static void setNim(int pnim) {
+        nim = pnim;
+    };
+
+    static int getNim() {
+        return nim;
+    }
+
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    };
+};
+
